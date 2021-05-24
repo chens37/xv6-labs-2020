72d18e7b807e85e13a1c5f07e889a320398f60f0
shichen <shichen.owner@outlook.com>
2021-05-24 21:53:42 +0800

    add memory allocator solution

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void *ksteal(int cpuid);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -19,15 +20,22 @@ struct run {
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+  struct spinlock lock[NCPU];
+  struct run *freelist[NCPU];
 } kmem;
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  int cid;
+
+  push_off();
+  cid = cpuid();
+  pop_off();
+
+  initlock(&kmem.lock[cid], "kmem");
+  if(cid == 0)
+    freerange(end, (void*)PHYSTOP);
 }
 
 void
@@ -46,6 +54,7 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  int cid;
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -55,13 +64,16 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  
+  push_off();
+  cid = cpuid();
+  acquire(&kmem.lock[cid]);
+  
+  r->next = kmem.freelist[cid];
+  kmem.freelist[cid] = r;
+  release(&kmem.lock[cid]);
+  pop_off();
 }
-
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
@@ -69,14 +81,68 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int cid;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  cid = cpuid();
+  acquire(&kmem.lock[cid]);
+  
+  r = kmem.freelist[cid];
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem.freelist[cid] = r->next;
+  else {
+    for(int i = 0;i < NCPU;i++){
+      if(cid == i)
+        continue;
+      acquire(&kmem.lock[i]);
+      if(kmem.freelist[i]){
+        r = kmem.freelist[i];
+        kmem.freelist[i] = r->next;
+        release(&kmem.lock[i]);
+        break;
+      }
+      release(&kmem.lock[i]);
+    }
+  }
+  release(&kmem.lock[cid]);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+//  else {
+//    r = (struct run*)ksteal(cid);
+//    if(r)
+//      memset((char*)r, 5, PGSIZE); // fill with junk
+//  }
+  pop_off();
   return (void*)r;
 }
+#if 0
+void *ksteal(int cid)
+{
+  int i;
+  struct run *r = 0;
+
+  for(i = 0;i < NCPU;i++)
+  {
+    if(cid == i)
+      continue;
+    acquire(&kmem.lock[i]);
+    if(kmem.freelist[i]) {
+      acquire(&kmem.lock[cid]);
+      for(int j = 0;j < 64;j++){
+        r = kmem.freelist[i];
+        kmem.freelist[i] = r->next;
+        r->next = kmem.freelist[cid];
+        kmem.freelist[cid] = r;
+        if(!kmem.freelist[i])
+          break;
+      }
+      r = kmem.freelist[cid];
+      kmem.freelist[cid] = r->next;
+      release(&kmem.lock[cid]);
+    }
+    release(&kmem.lock[i]);
+  }
+  return r;
+}
+#endif
