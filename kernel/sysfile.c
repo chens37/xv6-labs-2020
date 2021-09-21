19ed48b2bdd2dd6583533225c1912eff04a19bb8
shichen <shichen.owner@outlook.com>
2021-09-21 13:37:12 +0800

    finish symlink lab

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -254,6 +254,8 @@ create(char *path, short type, short major, short minor)
     ilock(ip);
     if(type == T_FILE && (ip->type == T_FILE || ip->type == T_DEVICE))
       return ip;
+	if((type == T_SYMLINK) && (ip->type == T_SYMLINK))
+	  return ip;
     iunlockput(ip);
     return 0;
   }
@@ -291,6 +293,7 @@ sys_open(void)
   struct file *f;
   struct inode *ip;
   int n;
+  int deep = 0;
 
   if((n = argstr(0, path, MAXPATH)) < 0 || argint(1, &omode) < 0)
     return -1;
@@ -314,6 +317,27 @@ sys_open(void)
       end_op();
       return -1;
     }
+	
+	while(ip->type == T_SYMLINK && omode != O_NOFOLLOW){
+	  if(deep > 10){
+		iunlockput(ip);
+		end_op();
+		return -1;
+	  }
+
+	  if(readi(ip, 0, (uint64)path, 0, MAXPATH) != MAXPATH){
+		iunlockput(ip);
+		end_op();
+		return -1;
+	  }
+	  iunlockput(ip);
+	  if((ip = namei(path)) == 0){
+	  	end_op();
+		return -1;
+	  }
+	  ilock(ip);
+	  deep++;
+	}
   }
 
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
@@ -484,3 +508,29 @@ sys_pipe(void)
   }
   return 0;
 }
+
+uint64
+sys_symlink(void)
+{
+	char target[MAXPATH],path[MAXPATH];
+	struct inode *ip;
+
+	if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+	  return -1;
+
+	begin_op();
+	ip = create(path, T_SYMLINK, 0, 0);
+	if(ip == 0){
+	  end_op();
+	  return -1;
+	}
+
+	if(writei(ip, 0, (uint64)target, 0, MAXPATH) != MAXPATH)
+	  panic("sys_symlink: writei");
+
+	iunlockput(ip);	
+	end_op();
+	return 0;	
+}
+
+
